785746ba214825c1fa8a1d54d99267c556eed7a2
Planfive <markyguila@hotmail.com>
2018-02-13 13:24:12 -0600

    added test suite, made some additions to DAStest and SGtest

---
+++ app/src/test/java/sep/myapplication/AllTests.java
@@ -0,0 +1,33 @@
+package sep.myapplication;
+
+/**
+ * Created by marky on 2018-02-13.
+ */
+import org.junit.Test;
+
+import sep.myapplication.business.ScrambleGenerator;
+
+public class AllTests {
+   @Test
+   public void testSuite() throws Exception{
+       DataAccessStubTest dasTest = new DataAccessStubTest();
+       ScrambleGeneratorTest sgTest = new ScrambleGeneratorTest();
+       TimerTest tTest = new TimerTest();
+
+        //Data Access Stub Tests
+       dasTest.initialValuesTest();
+       dasTest.dummyValuesTest();
+       dasTest.modifyDummyValuesTest();
+       dasTest.clearListTest();
+
+        //Scramble Generator Tests
+       sgTest.genScrambleTest();
+       sgTest.genRanNumTest();
+       sgTest.scrambleTranslationTest();
+
+        //Timer Tests
+       tTest.initialValuesTest();
+       tTest.processingTest();
+    }
+}
+
